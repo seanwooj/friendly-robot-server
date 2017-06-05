9ff943233f6e2b97b6a3f336f670420e8b746a68
Sean Kim <seanwoojinkim@gmail.com>
2017-06-04 21:25:32 -0700

    basic instantiation of serveR

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp'),
       fs = require('fs'),
+      path = require('path'),
       contentful = require('contentful'),
       SPACE_ID = '4bfme0qry1sf',
       ACCESS_TOKEN = '87b291853592493032ed043722c08071bab837cec4ca251c65227a5e66ab86e7',
-      SAVED_DB_FILE = './src/db/contentfulContent.json',
-      SAVED_PAGE_DB_FILE = './src/db/contentfulPageContent.json';
+      SAVED_PAGE_DB_FILE = './db/contentfulPageContent.json';
 
 
 
@@ -12,47 +12,6 @@ const gulp = require('gulp'),
 const folder = {
   src: 'src/'
 }
-
-gulp.task('sync-contentful', () => {
-  const client = createClient();
-  let db, token, syncPromise;
-
-  fs.readFile(SAVED_DB_FILE, function(err, data) {
-    if(err) {
-      return console.error(err);
-    }
-
-    db = JSON.parse(data);
-
-    // temporarily remove token assignment until we figure out how to
-    // remove old data, and update new data.
-    // this means initial: true will always be true, and will fetch all the data
-    // every time.
-    // token = db.nextSyncToken;
-
-    if(token) {
-      syncPromise = client.sync({
-        nextSyncToken: token
-      });
-    } else {
-      syncPromise = client.sync({
-        initial: true
-      });
-    }
-
-    syncPromise.then((response) => {
-      const writeableData = JSON.stringify(response);
-
-      fs.writeFile(SAVED_DB_FILE, writeableData,  function(err) {
-         if (err) { return console.error(err); }
-
-         console.log("Data written successfully!");
-      });
-    });
-
-  });
-});
-
 
 gulp.task('sync-pages', () => {
   const client = createClient();
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-import DataClient from 'data-client';
+import DataClient from './data-client';
 
 
 const app = express();
@@ -8,38 +8,16 @@ const dataClient = new DataClient();
 
 app.set('port', (process.env.API_PORT || 3001));
 
-// app.get('/api/check_token', (req, res) => {
-//   const token = extractToken(req);
-//
-//   if (token) {
-//     if (token === API_TOKEN) {
-//       return res.json({ valid: true });
-//     } else {
-//       return res.json({ valid: false });
-//     }
-//   } else {
-//     return res.status(400).json({
-//       valid: false,
-//       error: 'No token found in `Authorization` header',
-//     });
-//   }
-// });
-//
-// app.get('/api/albums', authenticatedRoute, (req, res) => {
-//   const albumIds = req.query.ids.split(',');
-//
-//   getAlbums(albumIds).then((albums) => (
-//     res.json(albums)
-//   )).catch((error) => (
-//     res.status(500).json({
-//       success: false,
-//       message: 'There was an error when interfacing with Spotify',
-//       error: error,
-//     })
-//   ));
-// });
 app.get('/api/pages', (req, res) => {
-  dataClient.
+  dataClient.fetchData().then((data) => {
+    res.json(data)
+  }).catch((error) => {
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong. get mad at sean for a useless error message',
+      error: error,
+    });
+  })
 });
 
 
